b45dfdc41d87279fb0ec6ef051bdd84351183c06
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-08-18 23:48:00 -0700

    finished js to end of day2

---
+++ js/storage.js
@@ -0,0 +1,1 @@
+var productImages = ['bag.jpg',"banana.jpg","bathroom.jpg","boots.jpg","breakfast.jpg","bubblegum.jpg","chair.jpg","cthulhu.jpg","dog-duck.jpg","dragon.jpg","pen.jpg","pet-sweep.jpg","scissors.jpg","shark.jpg","sweep.png","tauntaun.jpg","unicorn.jpg","usb.gif","water-can.jpg","wine-glass.jpg"]
